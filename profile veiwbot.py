060e346b8c3ff89c9ed7463a543e07063d84dc02
8a6u <102128935+8a6u@users.noreply.github.com>
2022-03-24 10:23:40 +1030

    GITHUB profile veiwbot!

---
+++ profile veiwbot.py
@@ -0,0 +1,311 @@
+from colorama import init, Fore, Back, Style
+import time
+import requests
+import time
+user = input('user:\n')
+urlib = f'https://gpvc.arturio.dev/{user}'
+from threading import Thread
+import random
+global num2
+num2 = 0
+def e1():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e2():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e3():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e4():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e5():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e6():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e7():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e8():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e9():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e10():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e11():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e12():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e13():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e14():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e15():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e16():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e17():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e18():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e19():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e20():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e21():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e22():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e23():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e24():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e25():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e26():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e27():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e28():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e29():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e30():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e31():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e32():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e33():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e34():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e35():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e36():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e37():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e38():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+def e39():
+    global num2
+    for x in range(1000):
+        r = requests.get(urlib)
+        num2 += 1
+        print(num2)
+import os
+def banner():
+    time.sleep(0.1)
+    global num2
+    print(num2)
+    qqq = 0
+    
+    
+    for x in range(100000):
+        
+        qqq += 1
+        if qqq >= 10:
+            qqq = 0
+        
+        os.system(f"color {qqq}")
+        
+                
+        print('''                                     
+██    ██ ███████ ██ ██     ██ 
+██    ██ ██      ██ ██     ██ 
+██    ██ █████   ██ ██  █  ██ 
+ ██  ██  ██      ██ ██ ███ ██ 
+  ████   ███████ ██  ███ ███ 
+            ''')
+        os.system(f"title sent:{num2} requests")
+
+for x in range(10):
+    Thread(target = e1).start()
+    Thread(target = e2).start()
+    Thread(target = e3).start()
+    Thread(target = e4).start()
+    Thread(target = e5).start()
+    Thread(target = e6).start()
+    Thread(target = e7).start()
+    Thread(target = e8).start()
+    Thread(target = e9).start()
+    Thread(target = e10).start()
+    Thread(target = e11).start()
+    Thread(target = e12).start()
+    Thread(target = e13).start()
+    Thread(target = e14).start()
+    Thread(target = e15).start()
+    Thread(target = e16).start()
+    Thread(target = e17).start()
+    Thread(target = e18).start()
+    Thread(target = e19).start()
+    Thread(target = e20).start()
+    Thread(target = e21).start()
+    Thread(target = e22).start()
+    Thread(target = e23).start()
+    Thread(target = e24).start()
+    Thread(target = e25).start()
+    Thread(target = e26).start()
+    Thread(target = e27).start()
+    Thread(target = e28).start()
+    Thread(target = e29).start()
+    Thread(target = e30).start()
+    Thread(target = e31).start()
+    Thread(target = e32).start()
+    Thread(target = e33).start()
+    Thread(target = e34).start()
+    Thread(target = e35).start()
+    Thread(target = e36).start()
+    Thread(target = e37).start()
+    Thread(target = e38).start()
+    Thread(target = e39).start()
+    Thread(target = banner).start()
